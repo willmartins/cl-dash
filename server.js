4600f3d68f38b6348a99bc7c02e1383d0beda14d
Nizatto <hello@nizatto.com>
2026-01-18 21:31:31 +0000

    Add Vercel config and export express app

--- server.js
+++ server.js
@@ -325,4 +325,8 @@ app.get('/api/shopify/callback/:type', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+export default app;
